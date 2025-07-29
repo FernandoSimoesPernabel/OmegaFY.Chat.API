6806004b495f8590ba7ac78e6849115a6e775c8d
Fernando Simões Pernabel <f.pernabel@influency.me>
2025-07-29 12:46:48 -0300

    feat: Adicionado projeto Benchmark para a classe de mensageria em memoria

---
+++ test/OmegaFY.Chat.API.Tests.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+using OmegaFY.Chat.API.Tests.Benchmark.Infra.MessageBus;
+
+namespace OmegaFY.Chat.API.Tests.Benchmark;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkDotNet.Reports.Summary summary = BenchmarkRunner.Run<InMemoryBusBenchmark>();
+    }
+}
