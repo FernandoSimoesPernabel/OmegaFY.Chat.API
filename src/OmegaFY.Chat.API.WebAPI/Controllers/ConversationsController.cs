d187b54a120e1ff488132502105ad1ad8d4efa15
Fernando Simões Pernabel <f_pernabel@hotmail.com>
2025-10-30 13:18:34 -0300

    chore: Criando as novas pastas

--- src/OmegaFY.Chat.API.WebAPI/Controllers/ConversationsController.cs
+++ src/OmegaFY.Chat.API.WebAPI/Controllers/ConversationsController.cs
@@ -1,6 +1,27 @@
-﻿namespace OmegaFY.Chat.API.WebAPI.Controllers;
+﻿using OmegaFY.Chat.API.Application.Shared;
+using OmegaFY.Chat.API.WebAPI.Models;
+
+namespace OmegaFY.Chat.API.WebAPI.Controllers;
 
 public sealed class ConversationsController : ApiControllerBase
 {
+    //[HttpPost()] //Creategroup
 
+    //[HttpPut("{conversationId:guid}/GrupoConfig")]
+
+    //[HttpPost("{conversationId:guid}/Members")]
+
+    //[HttpDelete("{conversationId:guid}/Members/{memberId:guid}")]
+
+    //[HttpGet("{conversationId:guid}/Messages/{messageId:guid}")]
+
+    //[HttpPost("{conversationId:guid}/Messages")]
+    //[ProducesResponseType(typeof(ApiResponse<>), StatusCodes.Status201Created)]
+    //[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+    //[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+    //public async Task<IActionResult> SendMessage([FromServices] object handler, object request, CancellationToken cancellationToken)
+    //{
+    //    HandlerResult<> result = await handler.HandleAsync(request.ToCommand(), cancellationToken);
+    //    return CreatedAtAction(nameof(GetFriendshipById), new { result.Data?.FriendshipId }, result);
+    //}
 }
