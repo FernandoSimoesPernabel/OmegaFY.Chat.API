55bd21114075e91271b8d13a800c3e3d8fe710a9
Fernando Simões Pernabel <f_pernabel@hotmail.com>
2026-02-18 20:28:49 -0300

    refactor: Instalando o AspNetCore.HealthChecks.Sqlite para usar o AddSqlite
    Replaced custom SQLite health check logic with the built-in AddSqlite extension from AspNetCore.HealthChecks.Sqlite for improved maintainability and standardization. Added the AspNetCore.HealthChecks.Sqlite NuGet package to project dependencies.

--- src/OmegaFY.Chat.API.Data.EF/Extensions/IHealthChecksBuilderExtensions.cs
+++ src/OmegaFY.Chat.API.Data.EF/Extensions/IHealthChecksBuilderExtensions.cs
@@ -1,24 +1,10 @@
-using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace OmegaFY.Chat.API.Data.EF.Extensions;
 
 public static class HealthCheckServiceCollectionExtensions
 {
     public static IHealthChecksBuilder AddSqliteHealthCheck(this IHealthChecksBuilder healthChecksBuilder, IConfiguration configuration)
-        => healthChecksBuilder.AddCheck("Sqlite", () =>
-        {
-            try
-            {
-                using SqliteConnection connection = new SqliteConnection(configuration.GetConnectionString("Sqlite"));
-                connection.Open();
-                return HealthCheckResult.Healthy("SQLite database is accessible.");
-            }
-            catch (Exception ex)
-            {
-                return HealthCheckResult.Unhealthy("SQLite database is not accessible.", ex);
-            }
-        }, tags: ["database", "storage", "sql"]);
+        => healthChecksBuilder.AddSqlite(configuration.GetConnectionString("Sqlite"), name: "SQLite", tags: ["database", "storage", "sql"]);
 }
