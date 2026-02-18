99e05fe18f701b44b5f1984c196fc8653008102c
Fernando Simões Pernabel <f_pernabel@hotmail.com>
2026-02-18 19:23:23 -0300

    refactor: Deixando no arquivo proprio

--- src/OmegaFY.Chat.API.Data.Dapper/Extensions/DependencyInjectionExtensions.cs
+++ src/OmegaFY.Chat.API.Data.Dapper/Extensions/DependencyInjectionExtensions.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OmegaFY.Chat.API.Application.Queries.QueryProviders.Chat;
 using OmegaFY.Chat.API.Application.Queries.QueryProviders.Users;
+using OmegaFY.Chat.API.Data.Dapper.Handlers;
 using OmegaFY.Chat.API.Data.Dapper.QueryProviders.Chat;
 using OmegaFY.Chat.API.Data.Dapper.QueryProviders.Users;
 using System.Data;
@@ -23,24 +24,5 @@ public static class DependencyInjectionExtensions
         services.AddScoped<IUserQueryProvider, UserQueryProvider>();
 
         return services;
-    }
-}
-
-internal sealed class GuidTypeHandler : SqlMapper.TypeHandler<Guid>
-{
-    public override Guid Parse(object value)
-    {
-        return value switch
-        {
-            string stringValue => Guid.Parse(stringValue),
-            Guid guidValue => guidValue,
-            _ => Guid.Empty
-        };
-    }
-
-    public override void SetValue(IDbDataParameter parameter, Guid value)
-    {
-        parameter.Value = value.ToString();
-        parameter.DbType = DbType.String;
     }
 }
